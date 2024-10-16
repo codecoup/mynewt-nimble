5e5540ef72bbe52dc45ba33172803540dde0e7b2
Andrzej Kaczmarek <andrzej.kaczmarek@codecoup.pl>
2024-10-16 23:00:54 +0200

    [xxx] nimble/ll: Add ext sync to padv and big events

--- nimble/controller/src/ble_ll_adv.c
+++ nimble/controller/src/ble_ll_adv.c
@@ -43,6 +43,7 @@
 #include "controller/ble_ll_utils.h"
 #include "controller/ble_ll_rfmgmt.h"
 #include "controller/ble_ll_iso_big.h"
+#include "controller/ext_sync.h"
 #include "ble_ll_conn_priv.h"
 #include "ble_ll_priv.h"
 
@@ -2447,6 +2448,14 @@ ble_ll_adv_periodic_schedule_first(struct ble_ll_adv_sm *advsm,
     BLE_LL_ASSERT(!advsm->periodic_sync[0].sch.enqueued);
     BLE_LL_ASSERT(!advsm->periodic_sync[1].sch.enqueued);
 
+    rc = ext_sync_anchor_get(advsm->padv_event_start, &advsm->padv_anchor,
+                             &advsm->padv_anchor_rem_us,
+                             advsm->padv_itvl_us - 10000 + 7750);
+    if (rc) {
+        advsm->padv_anchor_offset = 0;
+        first_pdu = false;
+    }
+
     advsm->periodic_sync_active = 1;
     advsm->periodic_sync_index = 0;
 
@@ -2723,6 +2732,7 @@ ble_ll_adv_sm_start_periodic(struct ble_ll_adv_sm *advsm)
     advsm->padv_anchor_offset = 1;
     advsm->padv_anchor = ble_ll_tmr_get();
     advsm->padv_anchor_rem_us = 0;
+    advsm->padv_event_start = advsm->padv_anchor;
 
     ble_ll_adv_sync_schedule(advsm, true);
 }
