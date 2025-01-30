ee42e3fca4df75d4acab72999f19876378ed8ff2
Andrzej Kaczmarek <andrzej.kaczmarek@codecoup.pl>
2025-01-30 16:34:41 +0100

    [xxx] nimble/phy: Add helpers to get configured phy mode
    This can be used in FEM callbacks to adjust TX power.

--- nimble/controller/include/controller/ble_phy.h
+++ nimble/controller/include/controller/ble_phy.h
@@ -212,6 +212,8 @@ void ble_phy_resolv_list_disable(void);
 
 #if (MYNEWT_VAL(BLE_LL_CFG_FEAT_LE_2M_PHY) || MYNEWT_VAL(BLE_LL_CFG_FEAT_LE_CODED_PHY))
 void ble_phy_mode_set(uint8_t tx_phy_mode, uint8_t rx_phy_mode);
+uint8_t ble_phy_mode_tx_get(void);
+uint8_t ble_phy_mode_rx_get(void);
 #endif
 
 static inline int ble_ll_phy_to_phy_mode(int phy, int phy_options)
